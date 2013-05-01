c65b5c913af1b967cdbe27866e69417091c4eb10
Sascha Schneider <development@suntsu.org>
2013-05-01 23:19:39 +0200

    Added redirect from / to /logbook

--- simple_logbook/urls.py
+++ simple_logbook/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^/$', RedirectView.as_view(url='/logbook/')),
     url(r'^logbook/', include('logbook.urls', namespace='logbook')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
